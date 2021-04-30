5b086cf383114c75ff89c8f320d04197e57aa344
师明骏 <8894481+shi-mingjun@user.noreply.gitee.com>
2021-04-30 15:16:07 +0800

    1111

--- src/views/detection2/index.vue
+++ src/views/detection2/index.vue
@@ -1,85 +1,109 @@
 <template>
   <div class="app-container">
     <div class="filter=container">
-      <el-input v-model="listQuery.name"
-                placeholder="漏洞名称"
-                style="width: 200px;"
-                class="filter-item"
-                @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.status"
-                 placeholder="类型"
-                 clearable
-                 style="width: 90px"
-                 class="filter-item">
-        <el-option v-for="item in statusOptions"
-                   :key="item"
-                   :label="item"
-                   :value="item" />
+      <el-input
+        v-model="listQuery.name"
+        placeholder="漏洞名称"
+        style="width: 200px;"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+      />
+      <el-select
+        v-model="listQuery.status"
+        placeholder="类型"
+        clearable
+        style="width: 90px"
+        class="filter-item"
+      >
+        <el-option
+          v-for="item in statusOptions"
+          :key="item"
+          :label="item"
+          :value="item"
+        />
       </el-select>
-      <el-button class="filter-item"
-                 type="primary"
-                 icon="el-icon-search"
-                 @click="handleFilter">
+      <el-button
+        class="filter-item"
+        type="primary"
+        icon="el-icon-search"
+        @click="handleFilter"
+      >
         搜索
       </el-button>
     </div>
-    <el-table v-loading="listLoading"
-              :data="list"
-              style="width:100%;"
-              border
-              fit
-              highlight-current-row
-              @sort-change="sortChange">
-      <el-table-column align="center"
-                       label="序号"
-                       width="100">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      style="width:100%;"
+      border
+      fit
+      highlight-current-row
+      @sort-change="sortChange"
+    >
+      <el-table-column
+        align="center"
+        label="序号"
+        width="100"
+      >
         <template slot-scope="{row}">
           <span>{{ row.id }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="漏洞名称"
-                       width="150"
-                       align="center">
+      <el-table-column
+        label="漏洞名称"
+        width="150"
+        align="center"
+      >
         <template slot-scope="{row}">
           {{ row.name }}
         </template>
       </el-table-column>
-      <el-table-column label="存在漏洞的页面"
-                       width="150"
-                       align="center">
+      <el-table-column
+        label="存在漏洞的页面"
+        width="150"
+        align="center"
+      >
         <template slot-scope="{row}">
           {{ row.url }}
         </template>
       </el-table-column>
-      <el-table-column label="漏洞描述"
-                       width="400"
-                       align="center">
+      <el-table-column
+        label="漏洞描述"
+        width="400"
+        align="center"
+      >
         <template slot-scope="{row}">
           <span>{{ row.description }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column label="解决方案"
-                       align="center">
+      <el-table-column
+        label="解决方案"
+        align="center"
+      >
         <template slot-scope="{row}">
           {{ row.method }}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col"
-                       label="危险等级"
-                       width="80"
-                       align="center">
+      <el-table-column
+        class-name="status-col"
+        label="危险等级"
+        width="80"
+        align="center"
+      >
         <template slot-scope="{row}">
           <el-tag :type="row.status | statusFilter">{{ row.status }}</el-tag>
         </template>
       </el-table-column>
 
     </el-table>
-    <pagination v-show="total>0"
-                :total="total"
-                :page.sync="listQuery.page"
-                :limit.sync="listQuery.limit"
-                @pagination="getList" />
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    />
   </div>
 </template>
 
@@ -87,7 +111,7 @@
 import { fetchList } from '@/api/leak'
 export default {
   filters: {
-    statusFilter (status) {
+    statusFilter(status) {
       const statusMap = {
         低: 'success',
         中: 'warning',
@@ -96,7 +120,7 @@ export default {
       return statusMap[status]
     }
   },
-  data () {
+  data() {
     return {
       tablekey: 0,
       list: null,
@@ -111,11 +135,11 @@ export default {
       statusOptions: ['高', '中', '低']
     }
   },
-  created () {
+  created() {
     this.getList()
   },
   methods: {
-    getList () {
+    getList() {
       this.listLoading = true
       fetchList(this.listQuery).then(response => {
         this.list = response.data.items
@@ -126,7 +150,7 @@ export default {
           this.listLoading = false
         }, 1.5 * 1000)
       })
-    }, handleFilter () {
+    }, handleFilter() {
       this.listQuery.page = 1
       this.getList()
     }
